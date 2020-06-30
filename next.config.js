3a863e94ade2f7d1a86368b0dbe3600a557bf7ec
roottool <smart.dog.lilienthal@gmail.com>
2020-06-30 11:50:01 +0000

    Fixed to execute HMR

--- next.config.js
+++ next.config.js
@@ -11,6 +11,18 @@ const nextConfig = {
 
     return config
   },
+  /**
+   * Hot Module Reloading with Next.js Docker development environment
+   * @url https://www.codemochi.com/blog/2019-08-27-nextjs-hmr
+   */
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+
+    return config
+  },
 }
 
 module.exports = withPlugins([[optimizedImages, {}]], nextConfig)
