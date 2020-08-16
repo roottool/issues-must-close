814b54c0a86063e544ec597bae0c64a160dab5d7
roottool <smart.dog.lilienthal@gmail.com>
2020-08-16 17:39:46 +0000

    Fixed alias

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@
 const withCSS = require('@zeit/next-css');
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
+const Path = require('path');
 
 const withCssSettings = withCSS({
   webpack(config) {
+    config.resolve.alias['@public'] = Path.join(__dirname, 'public');
+    config.resolve.alias['@src'] = Path.join(__dirname, 'src');
+
     config.module.rules.push({
       test: /\.tsx?$/,
       use: [
